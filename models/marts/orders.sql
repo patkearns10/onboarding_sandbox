ead040080eb1f49d5a8715f53810e6adde8dcb64
Jason Wei <jason.wei@chorus.co.nz>
2024-09-11 02:41:10 +0000

    Jason changes

---
+++ models/marts/orders.sql
@@ -0,0 +1,37 @@
+with orders as  (
+ 
+    select * from {{ ref('stg_jaffle_shop__orders' )}}
+ 
+),
+ 
+payments as (
+ 
+    select * from {{ ref('stg_payment') }}
+    
+),
+ 
+order_payments as (
+ 
+    select
+ 
+        order_id,
+        sum(case when status = 'success' then amount end) as amount_usd
+ 
+    from payments
+    group by 1
+),
+ 
+final as (
+ 
+    select
+        {{ dbt_utils.generate_surrogate_key(['order_id', 'customer_id'])}} as _pk,
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount_usd, 0) as amount
+ 
+    from orders
+    left join order_payments using (order_id)
+)
+ 
+select * from final
